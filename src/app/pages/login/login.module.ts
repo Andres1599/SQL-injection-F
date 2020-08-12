da2bbf1c190c2e918d77605dc1b489b0a962a577
Andres Higueros <higueros71@gmail.com>
2020-08-11 23:05:02 -0600

    add: agregando llamadas al api

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { UserService } from '../../services/user/user.service';
+import { AuthService } from '../../services/auth/auth.service';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [CommonModule, LoginRoutingModule],
+  providers: [UserService, AuthService],
   bootstrap: [LoginComponent],
 })
 export class LoginModule {}
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Users } from '../../models/users';
+import { environment } from '../../../environments/environment';
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient, private route: Router) {}
+
+  readUser(): Observable<Users[]> {
+    return this.http.get<Users[]>(environment.apiBase + '/users/');
+  }
+
+  createUser(user: Users): Observable<any> {
+    return this.http.post(environment.apiBase + 'user', user);
+  }
+
+  updateUser(user: Users): Observable<any> {
+    return this.http.put(environment.apiBase + 'user', user);
+  }
+
+  deleteUser(user: Users): Observable<any> {
+    return this.http.delete(environment.apiBase + 'user/' + user.id);
+  }
+
+  loginUser(user: Users): Observable<any> {
+    return this.http.post(environment.apiBase + 'user/login', user);
+  }
 }
